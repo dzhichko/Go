b52e7c3ce2b896e081d479866599c7e380207fcd
dzhichko <dzhichko.ao@edu.spbstu.ru>
2023-10-29 00:36:14 +0300

    try some changes in function

--- lab_2/cmd/file.go
+++ lab_2/cmd/file.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
+func main() {
+	arr := []int{3, 2, 1, 5, 6, 4}
+	fmt.Println(findKthLargest(arr, 2))
+}
+
 func findKthLargest(nums []int, k int) int {
 	nums = quickSort(nums)
-	return nums[len(nums)-k]
+	return nums[k-1]
 }
 
 func quickSort(arr []int) []int {
@@ -12,13 +19,13 @@ func quickSort(arr []int) []int {
 	start := arr[0]
 	var low, top []int
 	for _, num := range arr[1:] {
-		if num <= start {
-			low = append(low, num)
-		} else {
+		if num >= start {
 			top = append(top, num)
+		} else {
+			low = append(low, num)
 		}
 	}
-	res := append(quickSort(low), start)
-	res = append(res, quickSort(top)...)
+	res := append(quickSort(top), start)
+	res = append(res, quickSort(low)...)
 	return res
 }

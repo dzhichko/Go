ebfb06ef9be30e14fcc33a09026a6e050d6b615e
dzhichko <dzhichko.ao@edu.spbstu.ru>
2023-10-29 00:37:47 +0300

    forgot to delete main function

--- lab_2/cmd/file.go
+++ lab_2/cmd/file.go
@@ -1,12 +1,5 @@
 package main
 
-import "fmt"
-
-func main() {
-	arr := []int{3, 2, 1, 5, 6, 4}
-	fmt.Println(findKthLargest(arr, 2))
-}
-
 func findKthLargest(nums []int, k int) int {
 	nums = quickSort(nums)
 	return nums[k-1]

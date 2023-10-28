69bd9d210f0245d930da7fd5316dd55e659636a2
dzhichko <dzhichko.ao@edu.spbstu.ru>
2023-10-29 01:51:36 +0300

    try merge sort function

--- lab_2/cmd/file.go
+++ lab_2/cmd/file.go
@@ -1,15 +1,33 @@
 package main
 
 func findKthLargest(nums []int, k int) int {
-	insertionSort(nums)
+
+	nums = mergeSort(nums)
 	return nums[k-1]
 }
-func insertionSort(arr []int) {
-	for i := 1; i < len(arr); i++ {
-		for j := 0; j < i; j++ {
-			if arr[j] < arr[i] {
-				arr[j], arr[i] = arr[i], arr[j]
-			}
+
+func mergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	mid := len(arr) / 2
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	var result []int
+	for len(left) > 0 && len(right) > 0 {
+		if left[0] >= right[0] {
+			result = append(result, left[0])
+			left = left[1:]
+		} else {
+			result = append(result, right[0])
+			right = right[1:]
 		}
 	}
+	result = append(result, left...)
+	result = append(result, right...)
+	return result
 }

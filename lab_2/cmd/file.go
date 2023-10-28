dddc6abd3e08481b88919de254c5c5e2605af76c
dzhichko <dzhichko.ao@edu.spbstu.ru>
2023-10-28 22:31:07 +0300

    try insertion sort

--- lab_2/cmd/file.go
+++ lab_2/cmd/file.go
@@ -1,10 +1,21 @@
 package main
 
-import "sort"
-
 func findKthLargest(nums []int, k int) int {
 
-	sort.Ints(nums)
+	insertionSort(nums)
 
-	return nums[len(nums)-k]
+	return nums[k-1]
+}
+
+func insertionSort(arr []int) {
+	s := arr[0]
+	e := arr[len(arr)-1]
+
+	for i := s + 1; i < e; i++ {
+		j := i
+		for j > s && (j-1) > j {
+			arr[j-1], arr[j] = arr[j], arr[j-1]
+			j--
+		}
+	}
 }
